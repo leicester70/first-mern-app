a0b7af3354100a0db0005be8445c1b2e41900f05
C. L <82318965+leicester70@users.noreply.github.com>
2023-01-23 21:05:09 +0800

    typo

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(require("./routes/record"));
 
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require("./database/conn");
 
 app.listen(port, () => {
     // perform a database connection when server starts
